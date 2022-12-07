c0fa11a5edaac8b2a8849f7796d38d31613f5c87
CV418 <lp.error418@gmail.com>
2022-12-07 09:28:10 +0100

    création delete

--- userHandlers.js
+++ userHandlers.js
@@ -133,10 +133,29 @@ const getUser = (req, res) => {
     })
   }
 
+  const deleteUsers = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    database
+    .query("DELETE FROM users WHERE id = ?", [id])
+    .then(([result]) => {
+      if(result.affectedRows === 0) {
+        res.status(404).send("Not found");
+      } else {
+        res.sendStatus(204)
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user")
+    })
+  }
+
 module.exports = {
     getUsers,
     getUserById,
     getUser,
     postUsers,
     updateUsers,
+    deleteUsers,
 };
